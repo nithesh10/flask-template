849204ee22c28b11d39910e5c5a1597661f46773
nithesh10 <a.nitheshkumar@gmail.com>
2023-10-04 20:27:26 +0530

    first commit

---
+++ apps/__init__.py
@@ -0,0 +1,61 @@
+
+
+import os
+
+from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from importlib import import_module
+
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+
+
+def register_extensions(app):
+    db.init_app(app)
+    login_manager.init_app(app)
+
+
+def register_blueprints(app):
+    for module_name in ('authentication', 'home'):
+        module = import_module('apps.{}.routes'.format(module_name))
+        app.register_blueprint(module.blueprint)
+
+
+def configure_database(app):
+
+    @app.before_first_request
+    def initialize_database():
+        try:
+            db.create_all()
+        except Exception as e:
+
+            print('> Error: DBMS Exception: ' + str(e) )
+
+            # fallback to SQLite
+            basedir = os.path.abspath(os.path.dirname(__file__))
+            app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db.sqlite3')
+
+            print('> Fallback to SQLite ')
+            db.create_all()
+
+    @app.teardown_request
+    def shutdown_session(exception=None):
+        db.session.remove()
+
+from apps.authentication.oauth import github_blueprint
+
+def create_app(config):
+    app = Flask(__name__)
+    app.config.from_object(config)
+    config_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'config.py'))
+    config_path=config_path.replace("\\apps","")
+    #app.config.from_pyfile(config_path)
+    register_extensions(app)
+    register_blueprints(app)
+
+    app.register_blueprint(github_blueprint, url_prefix="/login") 
+    
+    configure_database(app)
+    return app
---
+++ apps/authentication/__init__.py
@@ -0,0 +1,10 @@
+
+
+
+from flask import Blueprint
+
+blueprint = Blueprint(
+    'authentication_blueprint',
+    __name__,
+    url_prefix=''
+)
